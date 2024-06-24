f321c216330b8e35e20345bf76048c1113563a48
Matheus Rossi Saciotto <saciotto@gmail.com>
2024-06-23 22:22:42 -0300

    Add base moviment

---
+++ Assets/Scripts/BaseManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    public float Speed = 0;
+    public GameObject SpawnPosition;
+    public GameObject DestroyPosition;
+
+    private void Start()
+    {
+        if (transform.position != SpawnPosition.transform.position) {
+            Instantiate(gameObject, SpawnPosition.transform.position, Quaternion.identity);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        MoveBase();
+        RemoveIfBaseIsOutOfTheScene();
+    }
+
+    void MoveBase()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, DestroyPosition.transform.position, Speed * Time.deltaTime);
+    }
+
+    void RemoveIfBaseIsOutOfTheScene()
+    {
+        if (transform.position == DestroyPosition.transform.position) {
+            Instantiate(gameObject, SpawnPosition.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
