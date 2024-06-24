2f297e4ed01ff6f960f2734ae2546c01ebd32b26
Matheus Rossi Saciotto <saciotto@gmail.com>
2024-06-24 20:47:07 -0300

    Gameplay improviments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,36 @@ public class PlayerController : MonoBehaviour
     public float Impulse = 5;
     public float MaxSpeed = 5;
 
+    bool impulseTriggered = false;
+    bool dead = false;
+
     void Update()
     {
-        // Impulse on mouse click or space key
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * Impulse, ForceMode2D.Impulse);
-        }
-
-        // Limit speed
-        if (GetComponent<Rigidbody2D>().velocity.y > MaxSpeed) {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, MaxSpeed);
+            impulseTriggered = true;
         }
     }
+
+    void FixedUpdate()
+    {
+        if (impulseTriggered && !dead) {
+            ImpulsePlayer();
+        }
+    }
+
+    void ImpulsePlayer()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, 0);
+        rb.AddForce(Vector2.up * Impulse, ForceMode2D.Impulse);
+        impulseTriggered = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle") {
+            dead = true;
+        }
+    }
+
 }
